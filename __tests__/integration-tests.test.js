e47611c3adf8c89f51beac9aec132a4c9ce5c636
NaveedGujral <naveedgujral93@gmail.com>
2023-11-22 10:01:06 +0000

    Q6 WIP

--- __tests__/integration-tests.test.js
+++ __tests__/integration-tests.test.js
@@ -33,6 +33,26 @@ describe("GET /api/topics tests", () => {
     })
 });
 
+describe("GET /api/articles/:article_id/comments tests", () => {
+        test('responds with array of objects with the correct keys. The array has the correct length and the accompanying code should be status 200', () => {
+            return request(app)
+            .get("/api/articles/3/comments")
+            .expect(200)
+            .then(( { body }) => {
+                expect(body.comments).toHaveLength(2)
+                body.comments.forEach((comment) => {
+                    expect(comment).toMatchObject({
+                      comment_id: expect.any(Number),  
+                      votes: expect.any(Number),  
+                      created_at: expect.any(String),  
+                      author: expect.any(String),  
+                      body: expect.any(String),  
+                      article_id: 3
+                })               
+            })
+        })
+    })
+});
 describe('GET /api tests', () => {
         test('should return an parsed JSON object with the correct information', () => {
             return request(app)
--- models/models.js
+++ models/models.js
@@ -9,7 +9,8 @@ exports.selectAllTopics = () => {
 }
 
 exports.selectAllCommentsByArtId = (artId) => {
-    return db.query(`SELECT comment_id, votes, created_at, author, body, article_id FROM comments WHERE article_id = $1 SORT BY created_at ASC;`, [artId])
+
+    return db.query(`SELECT comment_id, votes, created_at, author, body, article_id FROM comments WHERE article_id = $1 ORDER BY created_at ASC;`, [artId])
     .then(({ rows }) => {
         return rows
     })
