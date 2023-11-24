cbcd21acd953ff7a4df6b8b17ad3ed3c476baac6
NaveedGujral <naveedgujral93@gmail.com>
2023-11-24 14:26:59 +0000

    1) - put the old test back in that tested the object before the comment_count functionality was added

--- __tests__/integration-tests.test.js
+++ __tests__/integration-tests.test.js
@@ -144,6 +144,25 @@ describe('GET /api/articles/:article_id tests', () => {
                     body: 'I find this existence challenging',
                     created_at: '2020-07-09T20:11:00.000Z',
                     votes: 100,
+                    article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+                }
+            })
+        })
+    });
+    test('should respond with an object with the correct information including the comment_count property', () => {
+        return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+            expect(body).toMatchObject({
+                article: {
+                    article_id: 1,
+                    title: 'Living in the shadow of a great man',
+                    topic: 'mitch',
+                    author: 'butter_bridge',
+                    body: 'I find this existence challenging',
+                    created_at: '2020-07-09T20:11:00.000Z',
+                    votes: 100,
                     article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
                     comment_count: 11
                 }
